155a12cf91b842e545c32afd32a1a15e19d35e61
luisaabarros <luisaabarros1707@gmail.com>
2025-12-09 21:53:26 -0300

    Adiciona exercícios de função

--- aula08/exemplo03.py
+++ aula08/exemplo03.py
@@ -3,13 +3,21 @@
 #     print(f'Olá {n}, sua idade é {i}')
 
 
-def soma(t):
-    print(f'A soma dos numeros fornecidos dá: {t}.')
+# def soma(t):
+#     print(f'A soma dos numeros fornecidos dá: {t}.')
+
+# num1 = int(input('Informe o 1o numero: '))
+# num2 = int(input('Informe o 2o numero: '))
+# total = num1 + num2
+# soma(total)
+
+def soma(n1, n2):
+    total = n1 + n2
+    print(f'O total é: {total}')
 
 num1 = int(input('Informe o 1o numero: '))
 num2 = int(input('Informe o 2o numero: '))
-total = num1 + num2
-soma(total)
+soma(num1, num2)
 
 
 # nome = input('Informe seu nome: ')
