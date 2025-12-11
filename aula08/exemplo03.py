c9f139eb3b3d8bc64a2a1656f55ae1aa4a809a55
luisaabarros <luisaabarros1707@gmail.com>
2025-12-11 18:37:17 -0300

    Modifica estrutura do exemplo

--- aula08/exemplo03.py
+++ aula08/exemplo03.py
@@ -3,6 +3,13 @@
 #     print(f'Olá {n}, sua idade é {i}')
 
 
+# nome = input('Informe seu nome: ')
+# idade = int(input('Informe sua idade: '))
+
+# saudacao(nome, idade)
+
+
+
 # def soma(t):
 #     print(f'A soma dos numeros fornecidos dá: {t}.')
 
@@ -11,16 +18,15 @@
 # total = num1 + num2
 # soma(total)
 
-def soma(n1, n2):
-    total = n1 + n2
-    print(f'O total é: {total}')
-
-num1 = int(input('Informe o 1o numero: '))
-num2 = int(input('Informe o 2o numero: '))
-soma(num1, num2)
 
 
-# nome = input('Informe seu nome: ')
-# idade = int(input('Informe sua idade: '))
+# def soma(n1, n2):
+#     total = n1 + n2
+#     print(f'O total é: {total}')
 
-# saudacao(nome, idade)
+# num1 = int(input('Informe o 1o numero: '))
+# num2 = int(input('Informe o 2o numero: '))
+# soma(num1, num2)
+
+
+
