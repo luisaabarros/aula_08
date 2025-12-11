f5a2c8c7cad59b6f776725fe272762ae492f3528
luisaabarros <luisaabarros1707@gmail.com>
2025-12-11 20:26:26 -0300

    Adiciona ativades e novas funções

---
+++ aula08/atividade2.py
@@ -0,0 +1,12 @@
+# def vendas(a, b):
+#     t = a * b
+#     # print(f'O valor total das vendas é de {total}')
+#     return t  #Nada mais é executado depois do return, ou seja, se colocar alguma coisa depois NAO VAI RODAR
+
+
+# for n in range(3):
+#     preço = float(input('Qual o preço das vendas? '))
+#     qnt = int(input('Qual a quantidade de vendas? '))
+#     total = vendas(preço, qnt)
+#     print(f'O total é {total}')
+
--- aula08/exemplo03.py
+++ aula08/exemplo03.py
@@ -1,17 +1,47 @@
 # FUNÇÕES
-# def saudacao(n, i):
-#     print(f'Olá {n}, sua idade é {i}')
+
+def saudacao(n, i):
+    print(f'Olá {n}, sua idade é {i}')
+
+
+
+def soma(t):
+    print(f'A soma dos numeros fornecidos dá: {t}.')
+
+
+
+def soma(n1, n2):
+    total = n1 + n2
+    print(f'O total é: {total}')
+
+
+
+def maior_numero(a, b):
+    if a > b:
+        print(f'O numero maior é {a}')
+    elif a < b:
+        print(f'O numero maior é {b}')
+    else:
+        print('Os numeros são iguais')
+
+
+
+def somar_numeros(x, y):
+    total = x + y
+    print(f'O total é {total}')
+
+
+
+#---------------------------------- programa principal
 
 
 # nome = input('Informe seu nome: ')
 # idade = int(input('Informe sua idade: '))
-
 # saudacao(nome, idade)
 
 
+#---------------------------------- soma
 
-# def soma(t):
-#     print(f'A soma dos numeros fornecidos dá: {t}.')
 
 # num1 = int(input('Informe o 1o numero: '))
 # num2 = int(input('Informe o 2o numero: '))
@@ -19,14 +49,26 @@
 # soma(total)
 
 
+#---------------------------------- soma
 
-# def soma(n1, n2):
-#     total = n1 + n2
-#     print(f'O total é: {total}')
 
 # num1 = int(input('Informe o 1o numero: '))
 # num2 = int(input('Informe o 2o numero: '))
 # soma(num1, num2)
 
 
+#---------------------------------- descobre o maior
 
+
+# numero1 = float(input('Informe o primeiro numero: '))
+# numero2 = float(input('Informe o segundo numero: '))
+# maior_numero(numero1, numero2)
+
+
+#---------------------------------- somar 3 pares de numeros
+
+
+# for n in range(3):
+#     n1 = float(input('Digite o primeiro numero: '))
+#     n2 = float(input('Digite o segundo numero: '))
+#     somar_numeros(n1, n2)
